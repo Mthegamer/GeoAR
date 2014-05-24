fed53b756d418ab750d7c20a03cc47e423e18973
dannegm <danneg.m@gmail.com>
2014-05-23 20:11:27 -0500

    Integrando Google Maps

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -6,12 +6,41 @@ module.exports = [
         "clobbers": [
             "WikitudePlugin"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
+        "id": "org.apache.cordova.geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
+        "id": "org.apache.cordova.geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
+        "id": "org.apache.cordova.geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
+        "id": "org.apache.cordova.geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.wikitude.phonegap.WikitudePlugin": "3.3.1"
+    "com.wikitude.phonegap.WikitudePlugin": "3.3.1",
+    "org.apache.cordova.geolocation": "0.3.7"
 }
 // BOTTOM OF METADATA
 });
